684bd8fdbce3d36d9512d750b841446ae71475a6
GaneshGhatti <ghattiganesh8@gmail.com>
2024-09-24 16:21:48 +0530

    squirrel dummy animation

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { Canvas } from "@react-three/fiber";
+import { useRef, useState, useEffect } from "react";
+import "./App.css";
+import Home from "./pages/home/Home";
+import { OrbitControls, SpotLight } from "@react-three/drei";
+import { Squirrel } from "./components/Squirrel";
+
+function App() {
+  return (
+    <>
+      <div
+        className="w-screen h-screen fixed top-0 left-0 z-10 border"
+        id="squirrel-model"
+      >
+        <Canvas camera={{ position: [5, 5, 5], fov: 50 }} shadows>
+          <ambientLight intensity={4} />
+          <SpotLight
+            position={[10, 10, 10]}
+            angle={0.15}
+            penumbra={1}
+            intensity={1}
+            castShadow
+          />
+          <Squirrel />
+        </Canvas>
+      </div>
+      <Home />
+    </>
+  );
+}
+
+export default App;
