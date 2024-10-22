64da48848b2d283d5bad69ee8d7ae263b84618e0
Divyanshu <divyanshuvishwakarma222@gmail.com>
2024-10-23 03:02:05 +0530

    Add: Animation added on "About Section" and "Connect Section" And Add Banner component in "Footer"

--- src/components/About/AboutSection.jsx
+++ src/components/About/AboutSection.jsx
@@ -1,5 +1,6 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
+import Arrow from '../../assets/arrow.svg'
 
 const DISCRIPTION =
   "The Squirrel is a tech agency from India offering web, app development, and AI services. We create smart chatbots and digital solutions to boost efficiency and customer satisfaction. Stay ahead with our innovative, scalable technology. Contact us today!";
@@ -13,7 +14,7 @@ export default function AboutSection() {
       <div className=" flex flex-col gap-14 bg-white rounded-[50px] p-16 w-[90%]">
         <Title />
         <Description />
-        {/* <Button /> */}
+        <Button />
       </div>
     </section>
   );
@@ -36,10 +37,21 @@ function Description() {
 }
 
 function Button() {
+  const handleClick = (e) => {
+    e.preventDefault();
+    const element = document.getElementById("contact");
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
   return (
-    <button className=" text-nowrap bg-[#E8E8E8] flex flex-row items-center text-black w-full max-w-[700px]  border border-[#C8C8C8] rounded-full   px-5 py-2 gap-5">
-      <span className=" size-[60px] bg-black rounded-full"></span>
-      <span className="font-medium mb-2 text-7xl">CONTACT US!</span>
+    <button
+      onClick={handleClick}
+      className=" text-nowrap relative bg-[#E8E8E8] flex flex-row items-center text-black   w-fit md:w-full max-w-[700px]  border border-[#C8C8C8] rounded-full px-5  sm:px-[100px] py-2 sm:py-4 gap-5 group "
+    >
+
+      <img src={Arrow} className='hidden sm:block absolute left-5 group-hover:left-[calc(100%-75px)] transition-all duration-1000 ease-in-out group-hover:rotate-[400deg]' />
+      <span className="w-fit font-medium mb-2 text-4xl md:text-7xl">CONTACT US!</span>
     </button>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,16 @@ export default {
       fontFamily: {
         days: ['Days One', 'sans-serif'],
         darker: ['Darker Grotesque', 'sans-serif'],
-      }
+      },
+      animation: {
+        marquee: 'marquee 25s linear infinite ',
+      },
+      keyframes: {
+        marquee: {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-400%)' },
+        },
+      },
     },
   },
   plugins: [],
